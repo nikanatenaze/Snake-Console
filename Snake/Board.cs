6ac7abed985ab09f2085126653dbfa26a6f5445d
nika natenadze <nikanatenaze@gmail.com>
2025-01-15 22:53:27 +0400

    init
    Board draw system

---
+++ Snake/Board.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class Board
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Board() {
+            Width = 70;
+            Height = 25;
+        }
+
+
+        public void Write()
+        {
+            // With borders
+            for (int i = 0; i <= Width; i++)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.WriteLine("═");
+            }
+            for (int i = 0; i <= Width; i++)
+            {
+                Console.SetCursorPosition(i, Height);
+                Console.WriteLine("═");
+            }
+            // Height borders
+            for (int i = 0; i <= Height; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.WriteLine("║");
+            }
+            for (int i = 0; i <= Height; i++)
+            {
+                Console.SetCursorPosition(Width, i);
+                Console.WriteLine("║");
+            }
+            // Rounds
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("╔");
+            Console.SetCursorPosition(Width, 0);
+            Console.WriteLine("╗");
+            Console.SetCursorPosition(Width, Height);
+            Console.WriteLine("╝");
+            Console.SetCursorPosition(0, Height);
+            Console.WriteLine("╚");
+        }
+    }
+}
---
+++ Snake/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Snake;
+
+Console.WriteLine("Hello, World!");
+Board board =  new Board();
+board.Write();
